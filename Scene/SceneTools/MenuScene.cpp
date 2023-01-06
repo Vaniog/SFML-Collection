966ddd7dd1872ab8ab3eef8d95ab381bc6b239cf
Vaniog <forvantar@gmail.com>
2023-01-07 00:07:58 +0300

    Change CMake logic

--- Scene/SceneTools/MenuScene.cpp
+++ Scene/SceneTools/MenuScene.cpp
@@ -6,6 +6,14 @@ MenuScene::MenuScene() {
     cur_scene_index_ = 0;
     cur_scene_changed_ = false;
 
+    font_.loadFromFile("Resources/MenuFont.ttf");
+    help_text_.setFont(font_);
+    help_text_.setFillColor(sf::Color::White);
+    help_text_.setString("Arrows to select\n"
+                         "Enter to launch\n"
+                         "In play mode, ESC to exit");
+    help_text_.setPosition(0, 0);
+    help_text_.setCharacterSize(window_size_.y * char_size_);
     ReloadCover();
 }
 
@@ -21,6 +29,7 @@ void MenuScene::FillSceneNames() {
 void MenuScene::OnDraw(sf::RenderWindow& window) {
     cover_->OnDraw(window);
     old_cover_->OnDraw(window);
+    window.draw(help_text_);
 }
 
 void MenuScene::OnEvent(sf::Event& event, const Timer& timer) {
@@ -51,13 +60,15 @@ void MenuScene::ReloadCover() {
     if (cover_ != nullptr) {
         old_cover_ = cover_;
         old_cover_->MoveTo(-window_size_.x, window_size_.y / 2, 0.5);
+        cover_ = std::make_shared<ProjectCover>(scene_names_[cur_scene_index_]);
+        cover_->SetPosition(window_size_.x, window_size_.y / 2);
+        cover_->MoveTo(window_size_.x / 2, window_size_.y / 2, 0.3);
     } else {
         old_cover_ = std::make_shared<ProjectCover>(scene_names_[cur_scene_index_]);
         old_cover_->SetPosition(-window_size_.x, -window_size_.y);
+        cover_ = std::make_shared<ProjectCover>(scene_names_[cur_scene_index_]);
+        cover_->SetPosition(window_size_.x / 2, window_size_.y / 2);
     }
-    cover_ = std::make_shared<ProjectCover>(scene_names_[cur_scene_index_]);
-    cover_->SetPosition(window_size_.x, window_size_.y / 2);
-    cover_->MoveTo(window_size_.x / 2, window_size_.y / 2, 0.3);
 }
 
 std::string MenuScene::CurSceneName() {
